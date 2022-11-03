d51a7a5567b699380629dedf6cabf252f1854785
DenisDov <dovzdenis@gmail.com>
2022-11-03 13:27:26 +0200

    reactotron added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-/**
- * @format
- */
-
 import { AppRegistry, LogBox } from 'react-native';
 import App from './src/app/App';
 import { name as appName } from './app.json';
+
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
+}
 
 // TODO: https://github.com/Shopify/flash-list/issues/616
 LogBox.ignoreLogs([
--- src/services/api.ts
+++ src/services/api.ts
@@ -3,10 +3,10 @@ import { IProduct } from '../models/IProduct';
 
 export const dummyApi = createApi({
   reducerPath: 'dummyApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com' }),
   endpoints: builder => ({
     getAllProducts: builder.query<IProduct[], void>({
-      query: () => 'products',
+      query: () => '/products?limit=100',
       transformResponse: (rawResult: { products: IProduct[] }) => {
         return rawResult.products;
       },
