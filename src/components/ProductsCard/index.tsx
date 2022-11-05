71966805b088720d93d9e419c5cf9726171412b2
DenisDov <dovzdenis@gmail.com>
2022-11-05 10:28:01 +0200

    clean ProductsCard

--- src/components/ProductsCard/index.tsx
+++ src/components/ProductsCard/index.tsx
@@ -4,20 +4,18 @@ import { Dimensions } from 'react-native';
 import { Text, TouchBox, ImageBox } from '../../theme';
 
 import { capitalize } from '../../utils/capitalize';
-import { IProduct } from '../../models/IProduct';
+// import { IProduct } from '../../models/IProduct';
 
 const dummyImage = require('../../assets/images/dummy.jpeg');
 
 const { width } = Dimensions.get('window');
 
-const ProductsCard = ({ item: product, navigation }: IProduct) => {
-  // console.log('itemPRO: ', item);
-  // const navigation = useNavigation();
+const ProductsCard = ({ item: product, navigation }) => {
   const intlPrice = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
   }).format(product.price);
-  console.log('PRO12', product.id);
+
   return (
     <TouchBox
       onPress={() =>
