e1c9881a58f3a628c7eeaa5f3254a9ce648eddfb
DenisDov <dovzdenis@gmail.com>
2022-11-07 19:46:52 +0200

    upd skeleton

--- src/components/ProductsCard/Skeleton.tsx
+++ src/components/ProductsCard/Skeleton.tsx
@@ -17,14 +17,15 @@ const Skeleton = () => {
           width={width / 2 - GAP}
           height={200}
           viewBox="0 0 200 200"
-          // backgroundColor="#0F1115"
+          backgroundColor="#ccc"
           // foregroundColor="#ecebeb"
           style={styles.box}>
           <Rect x="8" y="0" rx="11" width="80" height="80" />
-          <Rect x="8" y="96" rx="3" ry="3" width="180" height="6" />
+          <Rect x="8" y="96" rx="3" ry="3" width="50" height="6" />
           <Rect x="8" y="114" rx="3" ry="3" width="150" height="6" />
-          <Rect x="8" y="133" rx="3" ry="3" width="130" height="6" />
-          <Rect x="8" y="151" rx="3" ry="3" width="40" height="6" />
+          <Rect x="8" y="133" rx="3" ry="3" width="180" height="6" />
+          <Rect x="8" y="151" rx="3" ry="3" width="180" height="6" />
+          <Rect x="8" y="169" rx="3" ry="3" width="80" height="6" />
         </ContentLoader>
       ))}
     </Box>
