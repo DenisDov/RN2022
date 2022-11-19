1d45887d29f2cff6bedc16f453dd8a0ac1e0b7a6
DenisDov <dovzdenis@gmail.com>
2022-11-19 14:41:21 +0200

    remove eact-native-media-console

--- src/screens/Home/SettingsStack/VideoScreen.tsx
+++ src/screens/Home/SettingsStack/VideoScreen.tsx
@@ -1,12 +1,11 @@
 import React, { useRef, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import VideoPlayer from 'react-native-media-console';
 import Video from 'react-native-video';
 
 // import Video from 'react-native-video';
 import { Box, Text } from '../../../theme';
 
-const VideoScreen = props => {
+const VideoScreen = () => {
   const [error, setError] = useState('');
   // const videoRef = useRef<Video>(null);
 
@@ -16,20 +15,6 @@ const VideoScreen = props => {
   return (
     <Box flex={1} backgroundColor="background" padding="m">
       <Box height={250}>
-        {/* <VideoPlayer
-        source={{ uri: 'https://vjs.zencdn.net/v/oceans.mp4' }}
-        navigator={props.navigation}
-        // style={styles.backgroundVideo}
-      /> */}
-        {/* <Video
-        // ref={videoRef}
-        source={{
-          uri: 'https://file-examples.com/storage/fe7589721d63762859d6962/2017/04/file_example_MP4_1920_18MG.mp4',
-        }}
-        onError={videoError}
-        style={styles.backgroundVideo}
-        repeat
-      /> */}
         <Video
           source={{
             uri: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
@@ -38,6 +23,7 @@ const VideoScreen = props => {
           controls={true}
           resizeMode="cover"
           hideShutterView={true}
+          onError={videoError}
           // paused={true}
         />
         {error && <Text>{error}</Text>}
