f157c37176ab82fdb508328a25c88d9854c6ad04
DenisDov <dovzdenis@gmail.com>
2022-12-05 18:09:24 +0200

    upd bottomSheet

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -8,7 +8,7 @@ const BSheet = () => {
   const theme = useTheme();
   const bottomSheetRef = useRef<BottomSheet>(null);
 
-  const snapPoints = useMemo(() => ['10%', '10%', '100%'], []);
+  const snapPoints = useMemo(() => [26, 26, '100%'], []);
 
   return (
     <Box flex={1} backgroundColor="surface">
@@ -19,8 +19,22 @@ const BSheet = () => {
         backgroundStyle={{
           backgroundColor: theme.colors.background,
         }}>
-        <Box flex={1} alignItems="center">
-          <Text>Awesome 🎉</Text>
+        <Box flex={1} alignItems="center" padding="m">
+          <Text>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
+            placeat voluptatibus quasi ipsa cum nulla architecto eveniet
+            repudiandae perferendis provident recusandae sit voluptas asperiores
+            eaque earum nobis vero delectus fuga ad, sint laboriosam animi. Ipsa
+            animi est saepe facere voluptas iusto doloribus laudantium, ab,
+            expedita excepturi molestias eum sequi accusamus rerum, debitis
+            autem quod sapiente et? Doloremque possimus aut temporibus
+            molestiae? Ducimus vitae dolorum quibusdam exercitationem. Enim
+            quisquam eos eum fuga ipsam doloremque molestias impedit
+            necessitatibus voluptatem asperiores maxime porro hic doloribus
+            harum temporibus, perspiciatis, qui dicta sunt nostrum cupiditate
+            aliquam nobis similique? Dolores, unde nemo? Laboriosam eaque dolor
+            quos.
+          </Text>
         </Box>
       </BottomSheet>
     </Box>
--- src/screens/Home/HomeStack/ProfileScreen.tsx
+++ src/screens/Home/HomeStack/ProfileScreen.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { BSheet } from '../../../components/BottomSheet';
 import { Header } from '../../../components/Header';
-import { Box } from '../../../theme';
+import { Box, Text } from '../../../theme';
 
 const ProfileScreen = () => {
   return (
     <Box flex={1} backgroundColor="surface">
       <Header textTitle="Profile" />
+      <Box padding="m">
+        <Text variant="title">Profile{'\n'}screen</Text>
+      </Box>
       <BSheet />
     </Box>
   );
