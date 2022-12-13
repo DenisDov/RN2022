ffb80a980a61cac762eea9d2f1bbccd44b415dba
DenisDov <dovzdenis@gmail.com>
2022-12-13 23:13:18 +0200

    fix gesture button with patch-package, remove moti products

--- src/screens/Home/HomeStack/ProductsScreen.tsx
+++ src/screens/Home/HomeStack/ProductsScreen.tsx
@@ -1,6 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
 import { FlashList } from '@shopify/flash-list';
-import { AnimatePresence, MotiView } from 'moti';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
@@ -9,18 +8,6 @@ import { Header } from '../../../components/Header';
 import { ProductsCard } from '../../../components/ProductsCard';
 import { useGetAllProductsQuery } from '../../../services/productsApi';
 import { Box, Text } from '../../../theme';
-
-const Skeleton = () => (
-  <MotiView
-    animate={{ opacity: 1 }}
-    exit={{
-      opacity: 0,
-    }}>
-    <Box padding="m">
-      <ActivityIndicator />
-    </Box>
-  </MotiView>
-);
 
 const ProductsScreen = () => {
   const { data: allProducts, isLoading, error } = useGetAllProductsQuery();
@@ -34,31 +21,25 @@ const ProductsScreen = () => {
           <Text>{error.toString()}</Text>
         </Box>
       )}
-      <AnimatePresence exitBeforeEnter>
-        {isLoading && <Skeleton key="skeleton" />}
-        {allProducts && (
-          <FlashList
-            data={allProducts}
-            renderItem={({ item }) => {
-              return (
-                <MotiView
-                  style={styles.motiView}
-                  key={item.id}
-                  from={{ opacity: 0, translateY: +item.id * 10 }}
-                  animate={{ opacity: 1, translateY: 0 }}>
-                  <ProductsCard item={item} navigation={navigation} />
-                </MotiView>
-              );
-            }}
-            estimatedItemSize={30}
-            numColumns={2}
-            ListEmptyComponent={<Text>no products available</Text>}
-            keyExtractor={item => item.id}
-            contentContainerStyle={styles.flashList}
-            showsVerticalScrollIndicator={false}
-          />
-        )}
-      </AnimatePresence>
+      {isLoading && !allProducts && (
+        <Box padding="m">
+          <ActivityIndicator />
+        </Box>
+      )}
+      {allProducts && (
+        <FlashList
+          data={allProducts}
+          renderItem={({ item }) => {
+            return <ProductsCard item={item} navigation={navigation} />;
+          }}
+          estimatedItemSize={30}
+          numColumns={2}
+          ListEmptyComponent={<Text>no products available</Text>}
+          keyExtractor={item => item.id}
+          contentContainerStyle={styles.flashList}
+          showsVerticalScrollIndicator={false}
+        />
+      )}
     </Box>
   );
 };
@@ -66,9 +47,6 @@ const ProductsScreen = () => {
 const styles = StyleSheet.create({
   flashList: {
     padding: 8,
-  },
-  motiView: {
-    flex: 1,
   },
 });
 
