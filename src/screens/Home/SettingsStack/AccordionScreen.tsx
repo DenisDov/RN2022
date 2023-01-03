aa0cfab01a219c825012564a8f7f2be18eebadc6
DenisDov <dovzdenis@gmail.com>
2023-01-03 18:23:03 +0200

    basic accordion

--- src/screens/Home/SettingsStack/AccordionScreen.tsx
+++ src/screens/Home/SettingsStack/AccordionScreen.tsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAnimatedStyle, withTiming } from 'react-native-reanimated';
 
 import { Header } from '../../../components/Header';
-import { Box, Text } from '../../../theme';
+import { Box, RectBox, Text } from '../../../theme';
+
+const SECTIONS = [
+  {
+    title: 'First',
+    content:
+      ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, recusandae.',
+  },
+  {
+    title: 'Second',
+    content:
+      ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, recusandae.',
+  },
+];
+
+type AccordionProps = {
+  item: {
+    title: string;
+    content: string;
+  };
+};
+
+const Accordion = ({ item }: AccordionProps) => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <Box>
+      <RectBox
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        backgroundColor="main"
+        onPress={() => setVisible(prev => !prev)}
+        padding="m">
+        <Text>{item.title}</Text>
+        <Text fontSize={32} lineHeight={32}>
+          {visible ? '-' : '+'}
+        </Text>
+      </RectBox>
+      {visible && (
+        <Box backgroundColor="background" padding="m">
+          <Text>{item.content}</Text>
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 const AccordionScreen = () => {
   return (
     <Box flex={1} backgroundColor="surface">
       <Header textTitle="RenderHtml" />
       <Box flex={1} padding="m">
-        <Text>AccordionScreen</Text>
+        {SECTIONS.map(item => (
+          <Accordion key={item.title} item={item} />
+        ))}
       </Box>
     </Box>
   );
