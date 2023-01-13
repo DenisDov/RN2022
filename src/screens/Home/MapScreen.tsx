1338f578e7bf636fa85a949eb6993ec6eaaa8db9
DenisDov <dovzdenis@gmail.com>
2023-01-13 11:57:45 +0200

    add scroll markers

--- src/screens/Home/MapScreen.tsx
+++ src/screens/Home/MapScreen.tsx
@@ -1,15 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { Dimensions, FlatList, ScrollView, StyleSheet } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE, Region } from 'react-native-maps';
 
 import { Box, Text } from '../../theme';
 
+const markers = [
+  {
+    id: 1,
+    name: 'Velopark',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quam necessitatibus numquam temporibus aliquid, nobis ea quis, aut porro doloremque perspiciatis voluptates dolor accusamus ad a illo. Vero, labore hic!',
+    latitude: 50.518830542287795,
+    longitude: 30.600759567944486,
+  },
+  {
+    id: 2,
+    name: 'MuzLine',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quam necessitatibus numquam temporibus aliquid, nobis ea quis, aut porro doloremque perspiciatis voluptates dolor accusamus ad a illo. Vero, labore hic!',
+    latitude: 50.509116186003205,
+    longitude: 30.61045769486924,
+  },
+  {
+    id: 3,
+    name: 'Metro',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quam necessitatibus numquam temporibus aliquid, nobis ea quis, aut porro doloremque perspiciatis voluptates dolor accusamus ad a illo. Vero, labore hic!',
+    latitude: 50.511898261486934,
+    longitude: 30.623931846165735,
+  },
+];
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
 const MapScreen = () => {
   const [region, setRegion] = useState<Region>({
-    latitude: 46.696228,
-    longitude: 32.544376,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    latitude: 50.51424523427609,
+    longitude: 30.613633014838392,
+    latitudeDelta: 0.09,
+    longitudeDelta: 0.04,
   });
 
   const handleRegionChange = (coords: Region) => {
@@ -25,14 +54,53 @@ const MapScreen = () => {
         initialRegion={region}
         onRegionChange={handleRegionChange}
         showsMyLocationButton>
-        <Marker
-          coordinate={{
-            latitude: 46.696228,
-            longitude: 32.544376,
-          }}
-        />
+        {markers.map(marker => {
+          return (
+            <Marker
+              key={marker.id}
+              coordinate={{
+                latitude: marker.latitude,
+                longitude: marker.longitude,
+              }}
+            />
+          );
+        })}
       </MapView>
+
       <Box
+        // backgroundColor="surface"
+        position="absolute"
+        left={0}
+        right={0}
+        bottom={0}>
+        <ScrollView
+          // contentContainerStyle={{ paddingHorizontal: 16 }}
+          horizontal
+          pagingEnabled
+          // decelerationRate="fast"
+          // snapToInterval={SCREEN_WIDTH - (64 - 16)} //your element width
+          // snapToAlignment="center"
+          bounces={false}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}>
+          {markers.map(marker => {
+            return (
+              <Box
+                key={marker.id}
+                width={SCREEN_WIDTH}
+                padding="m"
+                // marginHorizontal="s"
+                backgroundColor="error">
+                <Text marginBottom="s" fontSize={24}>
+                  {marker.name}
+                </Text>
+                <Text>{marker.description}</Text>
+              </Box>
+            );
+          })}
+        </ScrollView>
+      </Box>
+      {/* <Box
         backgroundColor="background"
         position="absolute"
         width={120}
@@ -49,7 +117,7 @@ const MapScreen = () => {
         elevation={2}>
         <Text>{region.latitude.toFixed(6)}</Text>
         <Text>{region.longitude.toFixed(6)}</Text>
-      </Box>
+      </Box> */}
     </Box>
   );
 };
